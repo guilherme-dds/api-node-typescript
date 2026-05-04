d1950b43183db385bb09e6fb94f28438d236b100
Guilherme <guilherme.dds.dev@gmail.com>
2026-05-04 12:53:53 -0300

    feat: added code style configs

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -3,8 +3,7 @@ import express from 'express';
 const server = express();
 
 server.get('/', (req, res) => {
+    return res.send('Olá, Dev!');
+});
 
-    return res.send('Olá, Dev!')
-})
-
-export { server }
+export { server };
