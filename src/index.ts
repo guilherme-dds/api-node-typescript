dc2417e23fad58b9029375d915642733e4337ea1
guilherme-dds <guilherme.dds.dev@gmail.com>
2026-06-10 12:10:46 -0300

    feat: environment variables and build

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { server } from './server/Server';
 
-server.listen(3333, () => console.log('Server rodando!'));
+server.listen(process.env.PORT || 3333, () => {
+    console.log(`Server rodando na porta ${process.env.PORT || 3333} `);
+});
